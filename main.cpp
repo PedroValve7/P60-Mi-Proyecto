1c4dfffc79a9d83f578dd26279a180e2e7c7a2a5
Sebastian Reinoso <jreinosor1@est.ups.edu.ec>
2022-05-31 10:34:32 -0500

    Creada la Clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "persona.h"
 using namespace std;
 
 int main()
 {
     cout << "Hola mundo en Git y Github!" << endl;
+    Persona p;
+    p.nombre = "Sebastian";
+    p.edad= 18;
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
